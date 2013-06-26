6e1ffd239c620c4eb96a6f1ca2fbec643be8a659
RyoutaMae <stephends@live.com>
2013-06-25 21:53:38 -0400

    Added colors class to util for easy access to commonly used colors

---
+++ common/oracle/util/OracleColors.java
@@ -0,0 +1,25 @@
+package oracle.util;
+
+import java.awt.Color;
+
+public final class OracleColors extends Color
+{
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+   
+   public static final Color TECH_GREEN = Color.decode("0x2AE843");
+   public static final Color SKY_BLUE = Color.decode("0x59D0E3");
+   public static final Color BRONZE = Color.decode("0xA1781A");
+   
+   /**
+    * This class should never be instantiated. As such, the instantiation is private to
+    * prevent this
+    * @param c
+    */
+   private OracleColors(int c) {
+      super(c);
+   }
+
+}
