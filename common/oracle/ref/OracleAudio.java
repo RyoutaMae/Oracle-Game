041d31a14bdbbf3a7ec9e7b1f121cc5851fb3550
RyoutaMae <stephends@live.com>
2013-07-30 00:54:20 -0400

    Reference class for audio files in oracle. The files currently use a direct path to the .WAV files. In the future they should use an abstract path. (not sure if that's the correct terminology. Will change if not.)

---
+++ common/oracle/ref/OracleAudio.java
@@ -0,0 +1,19 @@
+package oracle.ref;
+
+import java.io.File;
+
+public class OracleAudio
+{
+   public static File[] soundFiles = new File[3];
+
+   public static void setupOracleAudio()
+   {
+      soundFiles[0] = new File(
+            "D:\\Development\\Oracle\\source\\Oracle-Game\\resources\\menu.wav");
+      soundFiles[1] = new File(
+            "D:\\Development\\Oracle\\source\\Oracle-Game\\resources\\game.wav");
+      soundFiles[2] = new File(
+            "D:\\Development\\Oracle\\source\\Oracle-Game\\resources\\pop2.wav");
+   }
+
+}
