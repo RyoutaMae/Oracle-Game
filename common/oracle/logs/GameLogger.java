ab4c3ee645012f9b6e460523bfed98a69b0b2587
RyoutaMae <stephends@live.com>
2013-07-30 01:02:03 -0400

    Modified log naming system

--- common/oracle/logs/GameLogger.java
+++ common/oracle/logs/GameLogger.java
@@ -10,13 +10,14 @@ import javax.swing.JOptionPane;
 public class GameLogger
 {
    boolean qa = true;
-   static int logNum; 
+   static int logNum;
    File logFile;
    BufferedWriter log;
-   
+
    public GameLogger() {
       try {
-         logFile = new File("C:\\Users\\Public\\OracleLogs\\log" + Integer.toString(logNum) + ".log");
+         logFile = new File("C:\\Users\\Public\\OracleLogs\\log"
+               + Integer.toString(logNum) + ".log");
          log = new BufferedWriter(new FileWriter(logFile));
       } catch (IOException ex) {
          ex.printStackTrace();
@@ -25,26 +26,26 @@ public class GameLogger
       }
       logNum++;
    }
-   
 
-   public void logInput(int player, String input) throws IOException {
+   public void logInput(String player, String input) throws IOException
+   {
       if (qa) {
          log.write("Q: " + input);
          qa = false;
-         System.out.println("Logger: I logged a question!");
+         System.out.println("Logger: I logged a question from " + player + " !");
       } else {
          log.write("A: " + input);
          qa = true;
-         System.out.println("Logger: I logged an answer!");
+         System.out.println("Logger: I logged an answer from " + player + " !");
       }
-      
+
       log.newLine();
       log.newLine();
    }
-   
-   public void endLogging() throws IOException {
+
+   public void endLogging() throws IOException
+   {
       log.close();
    }
-   
 
 }
