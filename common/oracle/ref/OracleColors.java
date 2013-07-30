021267ed288d1f115e8db26495f031f797f6bfbc
RyoutaMae <stephends@live.com>
2013-07-30 00:55:03 -0400

    Reference class for Oracle Colors. Extends the basic java Color object and adds in custom colors

---
+++ common/oracle/ref/OracleColors.java
@@ -0,0 +1,27 @@
+package oracle.ref;
+
+import java.awt.Color;
+
+public final class OracleColors extends Color
+{
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+
+   public static final Color TECH_GREEN = Color.decode("0x2AE843");
+   public static final Color SKY_BLUE = Color.decode("0x59D0E3");
+   public static final Color BRONZE = Color.decode("0xA1781A");
+   public static final Color PALE_BLUE = Color.decode("0x598CE3");
+
+   /**
+    * This class should never be instantiated. As such, the instantiation is
+    * private to prevent this
+    * 
+    * @param c
+    */
+   private OracleColors(int c) {
+      super(c);
+   }
+
+}
