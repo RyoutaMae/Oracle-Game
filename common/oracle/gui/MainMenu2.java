1d75b31876d1cdd67bad13a888de78bb87201b47
RyoutaMae <stephends@live.com>
2013-07-30 00:59:56 -0400

    Nothing changed. Just removed extranious spaces.

--- common/oracle/gui/MainMenu2.java
+++ common/oracle/gui/MainMenu2.java
@@ -2,7 +2,6 @@ package oracle.gui;
 
 import java.awt.GridLayout;
 
-
 /**
  * This is the horizontal buttons menu prototype
  * 
@@ -13,11 +12,10 @@ public class MainMenu2 extends Menu
 
    public MainMenu2() {
       super();
-      
+
       buttonsPanel.setLocation(125, 280);
       buttonsPanel.setSize(550, 200);
       buttonsPanel.setLayout(new GridLayout(6, 0, 0, 10));
    }
-
 
 }
