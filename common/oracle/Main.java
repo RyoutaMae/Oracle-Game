4ba13d226cb3f6833fddeac80dda3657265f23c5
RyoutaMae <stephends@live.com>
2013-08-07 17:46:34 -0400

    Decided on vertical style menu and eliminated other

--- common/oracle/Main.java
+++ common/oracle/Main.java
@@ -1,9 +1,6 @@
 package oracle;
 
-import javax.swing.JOptionPane;
-
 import oracle.gui.MainMenu;
-import oracle.gui.MainMenu2;
 import oracle.ref.OracleAudio;
 
 public class Main
@@ -15,19 +12,9 @@ public class Main
    public static void main(String[] args)
    {
       OracleAudio.setupOracleAudio();
-      String[] options = { "Horizontal", "Vertical", "Exit" };
-      int option = JOptionPane.showOptionDialog(null, "ChooseLayout", "Layout",
-            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
-            null, options, null);
-      if (option == JOptionPane.NO_OPTION) {
-         @SuppressWarnings("unused")
-         MainMenu thing = new MainMenu();
-      } else if (option == JOptionPane.YES_OPTION) {
-         @SuppressWarnings("unused")
-         MainMenu2 thing2 = new MainMenu2();
-      } else {
-         System.exit(0);
-      }
+      @SuppressWarnings("unused")
+      MainMenu thing = new MainMenu();
+      
 
    }
 
