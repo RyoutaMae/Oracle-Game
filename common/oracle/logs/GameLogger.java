d80741c5f3daf1b18b2e70012dfca4026f48441a
RyoutaMae <stephends@live.com>
2013-12-15 02:42:33 -0500

    Logger now checks if there is already a log file under a name, and if there is, moves on to the next. Also, logger now creates the file directory for the logs.

--- common/oracle/logs/GameLogger.java
+++ common/oracle/logs/GameLogger.java
@@ -11,41 +11,46 @@ public class GameLogger
 {
    boolean qa = true;
    static int logNum;
-   File logFile;
-   BufferedWriter log;
+   private File logFile;
+   private BufferedWriter logger;
 
    public GameLogger() {
+      
       try {
-         logFile = new File("C:\\Users\\Public\\OracleLogs\\log"
-               + Integer.toString(logNum) + ".log");
-         log = new BufferedWriter(new FileWriter(logFile));
+         //Tests if the numbered log already exists, and if it does, tries the next number
+         do {
+            logFile = new File("C:\\Users\\Public\\OracleLogs\\log" + Integer.toString(logNum) + ".log");
+            logNum++;
+         } while (logFile.exists());
+         
+         //Makes the directory for the file if it does not already exist
+         logFile.getParentFile().mkdirs();
+         logger = new BufferedWriter(new FileWriter(logFile));
       } catch (IOException ex) {
          ex.printStackTrace();
          JOptionPane.showMessageDialog(null, "whoops");
          System.exit(0);
       }
-      logNum++;
+      
    }
 
    public void logInput(String player, String input) throws IOException
    {
       if (qa) {
-         log.write("Q: " + input);
+         logger.write(player + " Q: " + input);
          qa = false;
          System.out.println("Logger: I logged a question from " + player + "!");
       } else {
-         log.write("A: " + input);
+         logger.write("A: " + input);
          qa = true;
          System.out.println("Logger: I logged an answer from " + player + "!");
       }
-
-      log.newLine();
-      log.newLine();
+      logger.newLine();
    }
 
    public void endLogging() throws IOException
    {
-      log.close();
+      logger.close();
    }
 
 }
